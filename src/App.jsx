e1d981aa378fa42e6ed442cc62b5ef226fbc5c7a
Ganesh <ganeshgodse1902@gmail.com>
2024-12-05 15:12:24 +0530

    Started working on Navbar and Herosection

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+// import HeroSection from './components/HeroSection'
+
+function App() {
+ 
+
+  return (
+    <div className='w-full'>
+    
+     <Navbar />
+     {/* <HeroSection /> */}
+
+    </div>
+  )
+}
+
+export default App
